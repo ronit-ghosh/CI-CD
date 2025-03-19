213d4abe1cdd21a2ca591c47c979be39ced72893
ronit-ghosh <ronitghosh06@gmail.com>
2025-03-19 22:09:12 +0530

    Docker Compose Added

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,12 @@
-import User from "@/components/User";
-
 export default function Home() {
   return (
-    <User />
+    <>
+      <div className="">
+        Hello from Frontend
+      </div>
+      <div className="">
+        Hello again
+      </div>
+    </>
   );
 }
